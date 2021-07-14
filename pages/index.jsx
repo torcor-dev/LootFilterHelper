321d6229737fc81e0b0f0a600b04102895f7840e
Tormod Fossum <fossum@pm.me>
2021-07-14 21:41:15 +0200

    Move reducer, and state functionality to a utility file.

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,56 +6,23 @@ import { useSession} from 'next-auth/client'
 import { connectToDatabase } from '../utils/mongodb'
 import { useEffect, useReducer, useState } from 'react'
 import { getFilter } from '../utils/apiHelpers'
-
-const actions = {
-  SET_ID: "SET_ID",
-  SET_FILTER: "SET_FILTER",
-  SET_NAME: "SET_NAME",
-  SET_USERID: "SET_USERID",
-  RESET: "RESET",
-  START_LOADING: "START_LOADING",
-  FINISH_LOADING: "FINISH_LOADING",
-}
-
-function stateReducer(state, action) {
-  switch (action.type) {
-    case actions.SET_ID:
-      return { ...state, _id: action.payload.id }
-    case actions.SET_NAME:
-      return { ...state, name: action.payload.name }
-    case actions.SET_USERID:
-      return { ...state, userId: action.payload.userId }
-    case actions.RESET:
-      return { ...state, ...initialState }
-    case actions.START_LOADING:
-      return { ...state, isLoading: true }
-    case actions.FINISH_LOADING:
-      return { ...state, isLoading: false }
-  }
-}
-
-// Using this instead of BSON.ObjectId() because of BSON introducing
-// a SharedArrayBuffer, which will require cross-origin isolation.
-const ObjectId = (rnd = r16 => Math.floor(r16).toString(16)) =>
-    rnd(Date.now()/1000) + ' '.repeat(16).replace(/./g, () => rnd(Math.random()*16));
-
-const initialState = {
-  _id: "",
-  name: "",
-  userId: "",
-  isLoading: false,
-  isError: false,
-}
+import { dispatchWrapper, stateReducer, ObjectId, initialState } from '../utils/appState'
 
 export default function Home({ defaultFilter, staticData }) {
   const [session, loading] = useSession()
-  const [state, stateDispatch] = useReducer(stateReducer, initialState)
+  const [state, dispatch] = useReducer(stateReducer, initialState)
   const [filter, setFilter] = useState(defaultFilter)
   //const [updater, forceUpdate] = useReducer(x => x + 1, 0);
-  //
+  const {
+    startLoading, 
+    setId, 
+    setName, 
+    createId, 
+    finishLoading
+  } = dispatchWrapper(dispatch)
 
   useEffect(() => {
-    stateDispatch({ type: actions.START_LOADING })
+    startLoading()
     const existingFilter = localStorage.getItem("curFilter")
     if (existingFilter) {
       fetch(`api/filter/${existingFilter}`, {
@@ -65,18 +32,16 @@ export default function Home({ defaultFilter, staticData }) {
         }
       })
         .then(response => response.json())
-        .then(response => setFilter(response))
-        .then(
-          stateDispatch({type: "SET_ID", payload: {id: filter._id}}),
-          stateDispatch({type: "SET_NAME", payload: {name: filter.name}}),
-        )
+        .then(setFilter)
+        .then(setId(filter._id), setName(filter.name),)
+        .catch(e => console.log(e))
     } else {
-      stateDispatch({type: "SET_ID", payload: {id: ObjectId()}})
-      stateDispatch({type: "SET_NAME", payload: {name: "Default filter"}})
+      createId()
+      setName("Default Filter")
     }
 
    const timer = setTimeout(() => {
-      stateDispatch({ type: actions.FINISH_LOADING })
+      finishLoading()
     }, 500)
     return () => clearTimeout(timer)
   }, [filter._id, filter.name])
@@ -85,17 +50,17 @@ export default function Home({ defaultFilter, staticData }) {
   const [armourDetails] = staticData.filter(data => data.name === "armourDetails");
 
   function handleClick() {
-    stateDispatch({ type: actions.START_LOADING })
+    startLoading()
     fetch("api/filter/defaults/defaults")
       .then(response => response.json())
       .then(response => {
         localStorage.removeItem("curFilter")
         setFilter(response)
-        stateDispatch({type: "SET_ID", payload: {id: ObjectId()}})
-        stateDispatch({type: "SET_NAME", payload: {name: filter.name}})
+        createId()
+        setName("Default Filter")
       })
      setTimeout(() => {
-        stateDispatch({ type: actions.FINISH_LOADING })
+        finishLoading()
       }, 500)
     }
 

b3b558c2ea223914febf7b7013b8c037ccd5d79b
Tormod Fossum <fossum@pm.me>
2021-08-13 15:38:28 +0200

    Add handling for integer only inputs, external state handling.
    Manual handling of numeric inputs to limit the number of characters
    allowed. Currently we floor all numeric inputs, would need a
    modification to allow doubles, but there is currently no need. Added an
    optional callback for external state managment, unsure if that is bad
    practice, since the state of those values will be managed by two
    components.

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -1,43 +1,101 @@
 import PropTypes from 'prop-types'
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {synchCol} from '../utils/columnUtils'
 import {filterAPIPut} from "../utils/apiHelpers";
+import { uniqueId } from 'lodash';
 
-function InputField({ label, type="text", field, defaultValue="", filterId, className }) {
+function InputField({ 
+  label, 
+  type="text", 
+  title="",
+  field, 
+  defaultValue="", 
+  filterId, 
+  className, 
+  labelClassName="",
+  apiOptions=null,
+  setExternalState=null,
+  min=0,
+  max=100,
+}) {
   const [value, setValue] = useState(defaultValue)
+  const prefix = label ? label : "input_"
+  const [id] = useState(() => uniqueId(prefix))
+  // Warning: Prop `id` did not match. Server: "input16" Client: "input2"
 
-  useEffect(() => {
-    setValue(defaultValue);
-  }, [defaultValue]);
+  //useEffect(() => {
+  //  if (value === "") {
+  //    setValue(defaultValue);
+  //  }
+  //}, [value, defaultValue]);
   
   function onChange(e) {
+    if (type === "number" 
+      && (
+        e.target.value > max 
+        || e.target.value < min 
+        && e.target.value !== ""
+      )
+    ) {
+      return
+    }
+    if (type === "number" && e.target.value !== "") {
+      setValue(Math.floor(e.target.value))
+      return
+    }
     setValue(e.target.value)
   }
 
   function onBlur() {
-    filterAPIPut(field, value, filterId)
+    let v = value
+    if (type === "number" && v === "") {
+      setValue(min)
+      v = min
+    }
+    if (setExternalState) {
+      setExternalState(v)
+      console.log(v)
+    }
+    filterAPIPut(field, v, filterId, apiOptions)
+  }
+
+  function onFocus(e) {
+    e.target.select()
   }
 
   return (
     <>
-      {label && <label htmlFor={field}>{label.text}</label>}
+      {label && 
+        <label 
+          className={labelClassName} 
+          htmlFor={id}
+          title={title}
+        >
+          {label}
+        </label>}
       <input 
         type={type} 
-        id={field} 
+        title={title}
+        id={id} 
         value={value} 
         onBlur={onBlur}
         onChange={onChange}
         className={className ? className : ""}
+        onFocus={onFocus}
       />
     </>
   )
 }
 
 InputField.propTypes = {
-  label: PropTypes.object,
+  label: PropTypes.string,
   type: PropTypes.string,
   field: PropTypes.string,
-  value: PropTypes.string,
+  defaultValue: PropTypes.any,
+  filterId: PropTypes.string,
+  className: PropTypes.string,
+  labelClassName: PropTypes.string,
+  apiOptions: PropTypes.object,
 }
 
 export default InputField
